0b9f21c68e7548e75359aa5b8faad8b1581e643d
Seongjoo W <seongjoow@gmail.com>
2023-02-21 00:35:08 +0900

    BOJ #13305

--- BOJ13305.py
+++ BOJ13305.py
@@ -4,12 +4,12 @@ N = int(input()) # 도시의 개수
 road = list(map(int, sys.stdin.readline().split())) # 도로의 길이
 price = list(map(int, sys.stdin.readline().split())) # 리터당 가격
 
-
-
 pivot = price[0]
 result = 0
 
 for i in range(1, len(price)):
     result += road[i-1]*pivot
-    if price[i] > price[i+1]:
-        pivot = price[i+1] 
+    if pivot > price[i]:
+        pivot = price[i] 
+
+print(result)
