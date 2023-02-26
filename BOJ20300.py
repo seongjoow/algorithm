f7b6cdbd4c6695163fe7695d02baa1f4f248eb24
Seongjoo W <seongjoow@gmail.com>
2023-02-26 23:53:17 +0900

    BOJ #20300

---
+++ BOJ20300.py
@@ -0,0 +1,19 @@
+import sys
+
+N = int(input()) # 운동기구의 개수
+T = list(map(int, sys.stdin.readline().split())) # 각 운동기구의 근손실 정도
+
+T.sort()
+
+arr = []
+if N%2 == 1: #개수가 홀수일 때
+    for i in range(0, N//2):
+        sum = T[i] + T[-i-2]
+        arr.append(sum)
+    arr.append(T[-1])
+else: # 개수가 짝수일 때
+    for i in range(0, N//2):
+        sum = T[i] + T[-i-1]
+        arr.append(sum)
+
+print(max(arr))
