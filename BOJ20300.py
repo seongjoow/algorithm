80d3a80d38e718ef2ba49a95d74a1a393b6b829d
Seongjoo W <seongjoow@gmail.com>
2023-02-27 00:16:43 +0900

    BOJ #20300 풀이2

--- BOJ20300.py
+++ BOJ20300.py
@@ -6,12 +6,10 @@ T = list(map(int, sys.stdin.readline().split())) # 각 운동기구의 근손실
 T.sort()
 
 arr = []
-if N%2 == 1: #개수가 홀수일 때
-    for i in range(0, N//2):
-        arr.append(T[i] + T[-i-2])
-    arr.append(T[-1])
-else: # 개수가 짝수일 때
-    for i in range(0, N//2):
-        arr.append(T[i] + T[-i-1])
-
+if N%2 == 1:
+    m = T.pop(-1)
+    arr.append(m)
+for i in range(len(T)//2):
+	arr.append(T[i] + T[-i-1])
+    
 print(max(arr))
