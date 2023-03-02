935753d237f6130653749cd12006b3b2711f17f7
Seongjoo W <seongjoow@gmail.com>
2023-03-02 19:02:38 +0900

    BOJ #16953

---
+++ BOJ16953.py
@@ -0,0 +1,19 @@
+A, B = map(int, input().split())
+
+cnt = 0
+while A < B:
+    if A == B:
+        break
+    elif B%2 == 0:
+        B = B//2
+        cnt += 1
+    elif B%10 == 1:
+        B = B//10
+        cnt += 1
+    else:
+        break
+
+if A != B:
+    print(-1)
+else:
+    print(cnt+1)
