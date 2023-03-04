7e45f48a823aff6128f5448a8339af62c023b039
Seongjoo W <seongjoow@gmail.com>
2023-03-04 23:59:46 +0900

    BOJ #2606

---
+++ BOJ2606.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+v = int(input()) # 컴퓨터의 수
+e = int(input()) # 직접 연결된 컴퓨터 쌍의 수
+graph = [[] for _ in range(v+1)]
+visited = [0] * (v+1)
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(v):
+    visited[v] = 1
+    for nx in graph[v]:
+        if visited[nx] == 0:
+            dfs(nx)
+
+dfs(1)
+print(sum(visited) - 1)
