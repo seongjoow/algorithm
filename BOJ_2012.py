9c2ab0e03348e39c04f5fb186e4950329648f4eb
Seongjoo W <seongjoow@gmail.com>
2023-08-22 23:29:29 +0900

    BOJ #2012

--- BOJ_2012.py
+++ BOJ_2012.py
@@ -1,3 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input()) # 1 <= N <= 500,000
+rank = []
+
+for _ in range(N):
+    rank.append(int(input()))
+
+rank.sort()
+
+sum = 0
+for i in range(len(rank)):
+    sum += abs(rank[i]-(i+1))
+
+print(sum)
