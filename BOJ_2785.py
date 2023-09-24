78b8038d3655a1515f231b2ed7b081851d89f3a3
Seongjoo <seongjoow@gmail.com>
2023-09-25 01:12:21 +0900

    BOJ #2785

--- BOJ_2785.py
+++ BOJ_2785.py
@@ -7,7 +7,18 @@ input = sys.stdin.readline
 # 체인의 개수 (2 <= N <= 500,000)
 N = int(input())
 # 체인의 길이 (1 <= L[i] <= 1,000,000)
-for _ in range(N):
-    L = list(map(int, input().split()))
+L = list(map(int, input().split()))
 
+# 아이디어: 가장 짧은 체인부터 소모해가며 체인들을 연결.
+chains_to_connect = N
+chains_connected = 1
 
+L.sort()
+for chain in L:
+    if chains_connected + chain >= chains_to_connect:
+        break
+    else:
+        chains_connected += chain
+        chains_to_connect -= 1
+
+print(chains_to_connect - 1)
